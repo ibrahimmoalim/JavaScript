1dfda8b81afeebf4af661c75211e88a4f44ab1b7
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-02 12:05:58 +0300

    add exercise4 folder

---
+++ exercices/4/4.js
@@ -0,0 +1,39 @@
+/*
+function getLastValue(array){
+  const lastIndex=array.length -1
+  return array[lastIndex]
+}
+console.log(getLastValue([1,20,22,24,5]))
+console.log(getLastValue(['hi','hello','hey']))
+
+function arraySwap(array){
+  const i=array.length-1;
+  const lastValue= array[i];
+  const firstValue= array[0];
+  array[i]=firstValue;
+  array[0]=lastValue;
+  return array;
+}
+console.log(arraySwap([1,20,22,24,5]))
+console.log(arraySwap(['hi','hello','good']))
+*/
+
+/*
+for(n=5; n>=0; n-=1){
+  console.log(n)
+}
+*/
+
+let n=0;
+while(n<=10){
+  console.log(n)
+  n+=2
+}
+
+let h=5;
+while(h>=0){
+  console.log(h)
+  h-=1
+}
+
+

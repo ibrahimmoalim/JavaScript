a27d7c6a5db46783b77dcafc3f22885c5887ef0b
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-02 12:04:33 +0300

    modify

--- basic-projects/todo-list/todo-list.js
+++ basic-projects/todo-list/todo-list.js
@@ -4,14 +4,30 @@ function keyDown(){
   }
 }
 
+
 const todoList = [];
+
+displayTodoList()
+
+function displayTodoList(){
+  let todoListHTML='';
+
+  for(let i=0; i<todoList.length; i++){
+    const todo= todoList[i]
+    const html = `<p>${todo}</p>`;
+    todoListHTML+= html; 
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
 
 function todoAdd(){
   const inputElement= document.querySelector('.js-name-input');
   const name = inputElement.value;
 
   todoList.push(name);
-  console.log(todoList)
   
   inputElement.value = ''
+  displayTodoList()
 }

924dbe110d8597be0173b9252add6400f7bbad82
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-02 20:28:47 +0300

    add more exer

--- exercices/4/4.js
+++ exercices/4/4.js
@@ -70,7 +70,7 @@ console.log(addNum([1,2,3],3))
 console.log(addNum([-2,-1,0,99],2))
 */
 
-
+/*
 function countPositive(nums){
   let result4=0;
   for (let i=0; i<nums.length; i++){
@@ -82,3 +82,44 @@ function countPositive(nums){
 }
 console.log(countPositive([1,-3,5]))
 console.log(countPositive([2,-4,-5,-6,4,3,6,7]))
+*/
+
+
+function minMax(number){
+  const result5={
+    min: null,
+    max: null
+  }
+
+  for (let i=0; i<number.length; i++){
+    const value=number[i];
+
+    if (result5.min===null || value<result5.min){
+      result5.min=value;
+    }
+
+    if (result5.max===null || value>result5.max){
+      result5.max=value;
+    }
+  }
+  return result5;
+}
+
+console.log(minMax([]))
+console.log(minMax([1,4,2,-40,-6,-2,16]))
+
+
+function countWords(words){
+  const wordCount={}
+
+  for (let word of words){
+    if (wordCount[word]){
+      wordCount[word]++;
+    } else {
+      wordCount[word]=1;
+    }
+  }
+  return wordCount;
+}
+
+console.log(countWords(['apple','banana','apple','apple']))

b11e6d2bac8dd2f3de7d59e8c94e62c073fff1ae
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-11 11:46:07 +0300

    add comments

--- problemSolvingPractices/math-algorithms/fibonacci-sequence.js
+++ problemSolvingPractices/math-algorithms/fibonacci-sequence.js
@@ -1,16 +1,20 @@
-// Fibonacci n[i]= n[i-1]+n[1-2]
-
-
 function fibonacci(n){
-  fib = [0,1]  // create the first two numbers because without them fibonacci sequence is impossible
+  // We initialize the first two numbers because the Fibonacci recurrence requires two starting values.
+  fib = [0,1]
   for (let i=2; i<n; i++){
-    fib[i] = fib[i-2] + fib[i-1]  // Big-O = O(n)
+    fib[i] = fib[i-2] + fib[i-1]  // O(n)
   }
   return fib;  // O(1)
 }
+
+// Big-O = 0(n)
+
 console.log(fibonacci(2))  // output: [0,1]
 console.log(fibonacci(3))  // output: [0,1,1]
 console.log(fibonacci(9)) // output: [0,1,1,2,3,5,8,13,21]
+
+
+
 
 function fibonacci1(n){
   fib = [2,2]

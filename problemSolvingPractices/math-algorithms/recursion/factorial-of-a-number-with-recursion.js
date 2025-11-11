e0322e27b4ffbadf93988a8f2f51f1cfbf8d77d5
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-11 11:46:51 +0300

    add recursive factorial-of-n

---
+++ problemSolvingPractices/math-algorithms/recursion/factorial-of-a-number-with-recursion.js
@@ -0,0 +1,15 @@
+function recursiveFactorial(n){
+  if (n<1) return 1;  // base case
+  
+
+  return n * recursiveFactorial(n-1)
+}
+
+console.log(recursiveFactorial(0))
+console.log(recursiveFactorial(1))
+console.log(recursiveFactorial(2))
+console.log(recursiveFactorial(3))
+console.log(recursiveFactorial(4))
+console.log(recursiveFactorial(5))
+
+// Big-O = O(n), because number of operations grows with input size

69e8d56511f0bc047fd1f86e61138f5a2d4a3b38
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-16 18:52:54 +0300

    add climbing staircase problem

---
+++ problemSolvingPractices/algorithms/miscallaneous-problems/climbing-staircase.js
@@ -0,0 +1,17 @@
+// climbing staircase problem
+
+function climbingStaircase(n){
+  const noOfWays = [1, 2]
+  for (let i=2; i < n; i++){
+    noOfWays[i] = noOfWays[i-2] + noOfWays[i-1]
+  }
+  return noOfWays[n - 1]
+}
+
+console.log(climbingStaircase(1)) // 1
+console.log(climbingStaircase(2)) // 2
+console.log(climbingStaircase(3)) // 3
+console.log(climbingStaircase(4)) // 5
+console.log(climbingStaircase(5)) // 8
+
+// Big-O = O(n)

1089defee1794c4f56ee6e7730119ab9109bb721
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-16 18:02:12 +0300

    change Big-O comment

--- problemSolvingPractices/algorithms/miscallaneous-problems/cartesian-product.js
+++ problemSolvingPractices/algorithms/miscallaneous-problems/cartesian-product.js
@@ -16,4 +16,4 @@ function cartesianProduct(arr, arr2){
 console.log(cartesianProduct([1, 2], [3, 4, 5])) // [ [ 1, 3 ], [ 1, 4 ], [ 1, 5 ], [ 2, 3 ], [ 2, 4 ], [ 2, 5 ] ]
 
 // Big-O = O(nm) -> linear1*linear2, (array1 = O(n) & array2 = O(m)) since they have different length
-// if both arrays have same lenght, then Big-O = O(n) -> linear
+// if both arrays have same lenght, then Big-O = O(n^2) -> quadratic

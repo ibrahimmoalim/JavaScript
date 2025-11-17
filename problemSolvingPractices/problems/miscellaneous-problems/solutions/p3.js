3c46ad93f196f8613c53d38320a83aa55e79dec8
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-17 11:42:43 +0300

    add solution for p3

---
+++ problemSolvingPractices/problems/miscellaneous-problems/solutions/p3.js
@@ -0,0 +1,26 @@
+// Problem:
+// Reverse words in a sentence
+// Reverse the order of words, not the characters.
+
+// Example:
+// Input: "hello world from js"
+// Output: "js from world hello"
+
+
+function reverseWords(str){
+  const words = str.split(' ').reverse().join(' ');
+  return words
+}
+
+console.log(reverseWords('hello world from js'))  // js from world hello
+
+// Big-O = O(n) , becasue O(n)+O(n)+O(n) = O(n).
+
+// split -> breaks the string into arrays of words, using spaces "(' ')", new array after each space basically.
+// Big-O = O(n)
+
+// reverse() -> reverses the array of words we just created
+// Big-O = O(n)
+
+// join -> joins them back into a string, using spaces "(' ')"
+// Big-O = O(n)

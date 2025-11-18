379efadc557ecc5689ae7ada9a74d541c0c7bbe3
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-18 21:10:56 +0300

    add more exercises

--- exercices/5/5.js
+++ exercices/5/5.js
@@ -39,16 +39,66 @@
 // console.log(findSearch1(['lime','green','red','yellow','red','blue'], 'red')) // 2
 // console.log(findSearch1(['white','black','grey'], 'red')) // -1
 
-function removeFirstTwoEggs(arr){
+// function removeFirstTwoEggs1(arr){
+//   const result = []
+//   let eggsRemoved = 0
+//   for (let i=0; i < arr.length; i++){
+//     if(arr[i] === 'egg' && eggsRemoved < 2){
+//       eggsRemoved++
+//       continue
+//     }
+//     result.push(arr[i])
+//   }
+//   return result;
+// }
+// console.log(removeFirstTwoEggs1(['egg','apple','egg','egg','orange'])) // ['apple','egg','orange']
+
+// function removeFirstTwoEggs2(arr){
+//   const arrCopy = arr.slice();
+//   arrCopy.reverse();
+
+//   const result = [];
+//   let eggsRemoved = 0;
+//   for (let i=0; i < arrCopy.length; i++){
+//     if(arrCopy[i] === 'egg' && eggsRemoved < 2){
+//       eggsRemoved++;
+//       continue;
+//     }
+//     result.push(arrCopy[i]);
+//   }
+//   return result.reverse();
+// }
+// console.log(removeFirstTwoEggs2(['egg','apple','egg','egg','orange'])) // ['egg','apple','orange']
+
+
+function fizzBuzz(){
   const result = []
-  let eggsRemoved = 0
-  for (let i=0; i < arr.length; i++){
-    if(arr[i] === 'egg' && eggsRemoved < 2){
-      eggsRemoved++
+  for (let i=1; i <= 20; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+      result.push('FizzBuzz')
       continue
     }
-    result.push(arr[i])
+
+    if (i % 3 === 0){
+      result.push('Fizz')
+      continue
+    }
+
+    if (i % 5 === 0){
+      result.push('Buzz')
+      continue
+    }
+    result.push(i)
   }
   return result;
 }
-console.log(removeFirstTwoEggs(['egg','apple','egg','egg','orange'])) // ['apple','egg','orange']
+
+// console.log(fizzBuzz()) // [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz',16,17,'Fizz',19,'Buzz']
+
+
+function unique(arr){
+  return [...new Set(arr)];
+}
+
+console.log(unique(['red','green','green','red'])) // [ 'red', 'green' ]
+console.log(unique(['green','red','bule','red'])) // [ 'green', 'red', 'bule' ]

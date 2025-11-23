88504ce0c527992955d0ecf579c03e9a78d19565
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-23 17:58:52 +0300

    add amazon project

---
+++ basic-projects/amazon-project/script/script.js
@@ -0,0 +1,27 @@
+const products = [{
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  price: 10.90.toFixed(2)
+},
+{
+  image: 'images/products/intermediate-composite-basketball.jpg',
+  name: 'Intermediate Size Basketball',
+  rating: {
+    stars: 4,
+    count: 127
+  },
+  price: 20.95.toFixed(2)
+},
+{
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  price: 7.99.toFixed(2)
+}]

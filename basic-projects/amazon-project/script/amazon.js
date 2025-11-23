642607c73743fddb7c8cccf6014cf3596c70f486
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-23 19:05:48 +0300

    modify

--- basic-projects/amazon-project/script/amazon.js
+++ basic-projects/amazon-project/script/amazon.js
@@ -1,34 +1,7 @@
 // data structure
 // created with a compination of objects and arrays
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  price: 10.90
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  price: 20.95
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  price: 7.99
-}];
 
-let productsHTML = ''
+let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
@@ -51,7 +24,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${product.price.toFixed(2)}
+        $${(product.priceCents /100).toFixed(2)}
       </div>
 
       <div class="product-quantity-container">

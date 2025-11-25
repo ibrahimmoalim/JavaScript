121dd898c035f2641033e7d381b4ef5e4a626094
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-25 16:13:24 +0300

    save cart to local storage

--- basic-projects/amazon-project/data/cart.js
+++ basic-projects/amazon-project/data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart){
+  cart = [{
   productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
   quantity: 2
 },
@@ -6,6 +9,13 @@ export let cart = [{
   productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
   quantity: 4
 }];
+}
+
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId){
   const selectQuant = document.querySelector(`.js-select-quant-${productId}`)
@@ -27,6 +37,8 @@ export function addToCart(productId){
       quantity
     });
   };
+
+  saveToStorage();
 };
 
 
@@ -39,4 +51,5 @@ export function removeFromCart(itemId){
     }
   });
   cart = newCart;
+  saveToStorage();
 };

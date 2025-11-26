00ac11ac40605306979bff676249a580b36a1a95
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-11-26 21:11:40 +0300

    use default export

--- basic-projects/amazon-project/scripts/utils/price.js
+++ basic-projects/amazon-project/scripts/utils/price.js
@@ -1,3 +1,5 @@
-export function formatCurrency(price){
+function formatCurrency(price){
   return (price /100).toFixed(2);
 };
+// default export, used when there's only one export in the file
+export default formatCurrency;

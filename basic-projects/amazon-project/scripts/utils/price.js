4105c38da57f016050f026ad63914dbe43de412f
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-02 07:10:26 +0300

    create automated tests for formatCurrency

--- basic-projects/amazon-project/scripts/utils/price.js
+++ basic-projects/amazon-project/scripts/utils/price.js
@@ -1,5 +1,5 @@
 function formatCurrency(price){
-  return (price /100).toFixed(2);
+  return (Math.round(price) /100).toFixed(2);
 };
 // default export, used when there's only one export in the file
 export default formatCurrency;

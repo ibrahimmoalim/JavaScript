c3cbf6067fc6b4331626d9719c48d1378be333da
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-02 11:38:47 +0300

    add comments

--- basic-projects/amazon-project/data/products.js
+++ basic-projects/amazon-project/data/products.js
@@ -10,6 +10,8 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
+// data structure
+// created with a compination of objects and arrays
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",

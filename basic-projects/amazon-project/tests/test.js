4ad8253929ac1a3d045ed905d17b4236337b72fa
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-02 09:47:52 +0300

    rename file

--- basic-projects/amazon-project/tests/test.js
+++ basic-projects/amazon-project/tests/test.js
@@ -1,26 +1,28 @@
 import formatCurrency from '../scripts/utils/price.js';
 
 
-console.log('test suite: formatCurrency')
+
+
+// tests for formatCurrency function
+console.log('test suite: formastCurrency')
 
 console.log('converts cents to dollars')
-
 if (formatCurrency(2095) === '20.95') {
   console.log('passed')
 } else {
   console.log('failed')
 };
 
-console.log('works with 0')
 
+console.log('works with 0')
 if (formatCurrency(0) === '0.00') {
   console.log('passed')
 } else {
   console.log('failed')
 };
 
-console.log('rounds number before converting to dollars')
 
+console.log('rounds number before converting to dollars')
 if (formatCurrency(2000.5) === '20.01') {
   console.log('passed')
 } else {

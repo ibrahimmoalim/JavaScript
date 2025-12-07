597fba5024ec843e18886c9e2e95a1551e231bef
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-07 21:40:18 +0300

    add problem 2(find parrot height)

---
+++ problemSolvingPractices/problems/p2.js
@@ -0,0 +1,40 @@
+/*
+  let x = height of man
+  let y = height of parrot
+
+  x + y = 200
+
+  y = 200 - x
+
+  x = (200 - x) + 170
+
+  x - 170 = 200 - x
+
+  2x = 170 + 200
+
+  2x = 370
+
+  x = 185
+
+  y = x - 170
+
+  y = 185 - 170
+
+  y = 15
+*/
+
+
+let parrotHeight = 0
+
+let humanHeight = 0
+
+function calculateParrotHeight(totalHeight, humanMinusParrot){
+  
+  humanHeight = (totalHeight + humanMinusParrot) /2
+
+  parrotHeight = humanHeight - humanMinusParrot
+
+  console.log(parrotHeight) // 15
+}
+
+calculateParrotHeight(200, 170)

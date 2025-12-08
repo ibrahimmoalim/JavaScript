a2d6a8758fdb2b89e22fe977f35796415dc18386
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-08 19:33:22 +0300

    make place order button interactive

--- basic-projects/amazon-project/scripts/checkout/payment-summary.js
+++ basic-projects/amazon-project/scripts/checkout/payment-summary.js
@@ -2,23 +2,24 @@ import { cart } from "../../data/cart-class.js"
 import { getProduct } from "../../data/products.js";
 import formatCurrency from "../utils/price.js";
 import { getDeliveryOption } from "../../data/delivery-options.js";
+import { addOrder } from "../../data/orders.js";
 
 
 
 
 export function renderPaymentSummary(){
 
-let paymentHTML = document.querySelector('.js-payment-summary')
+  let paymentHTML = document.querySelector('.js-payment-summary')
 
-if (cart.cartItems.length === 0){
-  paymentHTML.remove();
-  document.querySelector('.js-page-title').innerHTML = 'Cart is empty'
-} else {
+  if (cart.cartItems.length <= 0){
+    paymentHTML.remove();
+    document.querySelector('.js-page-title').innerHTML = 'Cart is empty'
+  } else {
 
-  let itemsPriceCents = 0;
-  let deliveryShipping = 0;
+    let itemsPriceCents = 0;
+    let deliveryShipping = 0;
 
-  cart.cartItems.forEach((cartItem) => {
+    cart.cartItems.forEach((cartItem) => {
 
       const product = getProduct(cartItem.productId)
       itemsPriceCents += product.priceCents * cartItem.quantity;
@@ -27,13 +28,13 @@ if (cart.cartItems.length === 0){
       deliveryShipping += deliveryOption.priceCents;
     });
 
-  const totalBeforeTax = formatCurrency(itemsPriceCents + deliveryShipping)
-  const tax = totalBeforeTax /10
-  const orderTotal = (Number(totalBeforeTax) + tax)
+    const totalBeforeTax = formatCurrency(itemsPriceCents + deliveryShipping)
+    const tax = totalBeforeTax /10
+    const orderTotal = (Number(totalBeforeTax) + tax)
 
 
 
-  
+    
     paymentHTML.innerHTML = `
 
       <div class="payment-summary-title">
@@ -75,10 +76,39 @@ if (cart.cartItems.length === 0){
         </div>
       </div>
 
-      <button class="place-order-button button-primary">
+      <button class="place-order-button button-primary js-place-order">
         Place your order
       </button>
 
     `
-}
-}
+
+    document.querySelector('.js-place-order')
+      .addEventListener('click', async () => {
+
+        try {
+
+          const response = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              cart: cart
+            })
+          });
+
+          const order = await response.json()
+          addOrder(order);
+
+        } catch (error) {
+
+          console.error('Unexpected error, try again later.')
+          console.error(error)
+          
+        }
+
+        window.location.href = 'orders.html';
+    });
+  };
+
+};

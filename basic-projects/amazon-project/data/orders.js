65f0dc2bd19c530525cf93d04732dedf74b6ac15
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-08 19:33:56 +0300

    generate orders page html with js

---
+++ basic-projects/amazon-project/data/orders.js
@@ -0,0 +1,109 @@
+export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order){
+  orders.unshift(order)
+  saveToStorage()
+};
+console.log(JSON.stringify(orders))
+
+function saveToStorage(){
+  localStorage.setItem('orders', JSON.stringify(orders))
+};
+
+
+generateOrdersHTML();
+
+function generateOrdersHTML(){
+  let ordersHTML = document.querySelector('.js-orders-grid')
+
+  
+  if (!ordersHTML) return; // This module runs on multiple pages. Skip rendering unless the Orders UI (ordersHTML) exists.
+
+  if (orders.length <= 0){
+    ordersHTML.remove()
+    document.querySelector('.js-orders-page-title').innerHTML = 'You have no orders'
+  } else {
+
+    ordersHTML.innerHTML += `
+    
+      <div class="order-container">
+          
+          <div class="order-header">
+            <div class="order-header-left-section">
+              <div class="order-date">
+                <div class="order-header-label">Order Placed:</div>
+                <div>August 12</div>
+              </div>
+              <div class="order-total">
+                <div class="order-header-label">Total:</div>
+                <div>$35.06</div>
+              </div>
+            </div>
+
+            <div class="order-header-right-section">
+              <div class="order-header-label">Order ID:</div>
+              <div>${orders[0].id}</div>
+            </div>
+          </div>
+
+          <div class="order-details-grid">
+            <div class="product-image-container">
+              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            </div>
+
+            <div class="product-details">
+              <div class="product-name">
+                Black and Gray Athletic Cotton Socks - 6 Pairs
+              </div>
+              <div class="product-delivery-date">
+                Arriving on: August 15
+              </div>
+              <div class="product-quantity">
+                Quantity: 1
+              </div>
+              <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+              </button>
+            </div>
+
+            <div class="product-actions">
+              <a href="tracking.html">
+                <button class="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+
+            <div class="product-image-container">
+              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
+            </div>
+
+            <div class="product-details">
+              <div class="product-name">
+                Adults Plain Cotton T-Shirt - 2 Pack
+              </div>
+              <div class="product-delivery-date">
+                Arriving on: August 19
+              </div>
+              <div class="product-quantity">
+                Quantity: 2
+              </div>
+              <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+              </button>
+            </div>
+
+            <div class="product-actions">
+              <a href="tracking.html">
+                <button class="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+          </div>
+      </div>
+    `
+  }
+};

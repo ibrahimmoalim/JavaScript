db8fd9a13b9ff600f09fb406333b7955e74b9b96
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-09 18:47:46 +0300

    remove comment

--- basic-projects/amazon-project/scripts/checkout.js
+++ basic-projects/amazon-project/scripts/checkout.js
@@ -1,6 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
-// import '../data/cart-class.js'; // imports all code from said file
 
 renderOrderSummary();
 renderPaymentSummary();

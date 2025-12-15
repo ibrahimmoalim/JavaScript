26e3056a85383e85225fbc4138344ecb1ec9949c
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-15 22:26:19 +0300

    rename

---
+++ problemSolvingPractices/problems/p1-highestNumber.js
@@ -0,0 +1,44 @@
+// find the largest, smallest, second largest and third largest number in an array of numbers
+
+function highestNum(nums) {
+  
+  let max = -Infinity, second = -Infinity, third = -Infinity, min = Infinity;
+
+  if (nums.length === 0) {
+    max = null, min = null, second = null, third = null
+  }
+
+  for (let num of nums) {
+
+    if (num > max) {
+      third = second
+      second = max
+      max = num
+    } else if (num < max && num > second) {
+      third = second
+      second = num
+    } else if (num < second && num > third) {
+      third = num
+    }
+
+    if (num < min) {
+      min = num
+    }
+  }
+
+  return { max, second, third, min };
+}
+console.log(highestNum([34, 4, 71, 2, 99, 34, 2, -34, -8, 99, 4]))
+
+// Big-O = O(n)
+
+/*
+function summation(n){
+  let sum =0
+  for (i of n){
+    sum += i
+  }
+  return sum;
+}
+console.log(summation([34,2,3,4]))
+*/
---
+++ problemSolvingPractices/problems/p2-ParrotHeight.js
@@ -0,0 +1,43 @@
+/*
+  let x = height of man
+  let y = height of parrot
+
+  x + y = 200
+
+  y = 200 - x
+
+  x = (200 - x) + 170
+
+  x - 170 = 200 - x
+
+  2x = 170 + 200
+
+  2x = 370
+
+  x = 185
+
+  y = x - 170
+
+  y = 185 - 170
+
+  y = 15
+*/
+
+
+let parrotHeight;
+
+let humanHeight;
+
+function calculateParrotHeight(totalHeight, humanMinusParrot){
+  
+  humanHeight = (totalHeight + humanMinusParrot) /2
+
+  parrotHeight = humanHeight - humanMinusParrot
+
+  console.log(parrotHeight)
+}
+
+calculateParrotHeight(200, 170) // 15
+calculateParrotHeight(300, 150) // 75
+calculateParrotHeight(250, 190) // 30
+calculateParrotHeight(150, 85) // 32.5
